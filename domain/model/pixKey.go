618b81ec925f89e2cd48f3f5aa12a6349890aa16
Ruan Scherer <ruan.scherer@paytrack.com.br>
2023-10-29 15:04:15 -0300

    create domain models

---
+++ domain/model/pixKey.go
@@ -0,0 +1,58 @@
+package model
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type PixKeyRepository interface {
+	RegisterKey(pixKey *PixKey) (*PixKey, error)
+	FindKeyByKind(key string, kind string) (*PixKey, error)
+	AddBank(bank *Bank) error
+	AddAccount(account *Account) error
+	FindAccount(id string) (*Account, error)
+}
+
+type PixKey struct {
+	Base      `valid:"required"`
+	Kind      string   `json:"kind" valid:"notnull"`
+	Key       string   `json:"key" valid:"notnull"`
+	AccountID string   `json:"account_id" valid:"notnull"`
+	Account   *Account `valid:"-"`
+	Status    string   `json:"status" valid:"notnull"`
+}
+
+func (pk *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pk)
+
+	if pk.Kind != "email" && pk.Kind != "cpf" {
+		return errors.New("invalid type of key")
+	}
+
+	if pk.Status != "active" && pk.Status != "inactive" {
+		return errors.New("invalid status")
+	}
+
+	return err
+}
+
+func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
+	pixKey := &PixKey{
+		Kind:    kind,
+		Key:     key,
+		Account: account,
+		Status:  "active",
+	}
+
+	pixKey.ID = uuid.NewV4().String()
+	pixKey.CreatedAt = time.Now()
+
+	err := pixKey.isValid()
+	if err != nil {
+		return nil, err
+	}
+	return pixKey, nil
+}
